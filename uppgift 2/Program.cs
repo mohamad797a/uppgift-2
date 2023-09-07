404a0ab3456bbeadcd83a6cca510cb761125e2ac
Pingvu <nhlpool27@gmail.com>
2023-09-07 13:15:27 +0200

    Add project files.

---
+++ uppgift 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace uppgift_1._1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("system 32 error chek drivers");
+            Console.ReadKey();
+        }
+    }
+}
+
+
